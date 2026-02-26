b1d797f7f1341b5ff6b0b18eb25ad156d932bec2
Emriel <emirhan93@windowslive.com>
2026-02-26 17:05:12 +0300

    There are lots of changes because of review that I take for spring core task. Because of that I choose to re-do hiberante task instead of do the same refactoring phase for task1. I will upload src code from task1 and continue with that.

--- src/main/java/com/epam/springCoreTask/util/PasswordGenerator.java
+++
@@ -1,5 +0,0 @@
-package com.epam.springCoreTask.util;
-
-public interface PasswordGenerator {
-    String generatePassword();
-}
--- src/main/java/com/epam/springCoreTask/util/UsernameGenerator.java
+++
@@ -1,7 +0,0 @@
-package com.epam.springCoreTask.util;
-
-import java.util.List;
-
-public interface UsernameGenerator {
-    String generateUsername(String firstName, String lastName, List<String> existingUsernames);
-}
